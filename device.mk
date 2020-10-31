ce1b5960e0422b06fa65ef8328e7e6d4d4f813b6
Bruno Martins <bgcngm@gmail.com>
2020-10-31 20:39:38 +0000

    RMX2061: Fix apex mounting [    2.944007]  (4)[528:init]logwrapper: executing /apex/com.android.runtime/bin/linkerconfig failed: No such file or directory [    2.944009]  (6)[1:init]linkerconfig: executing /apex/com.android.runtime/bin/linkerconfig failed: No such file or directory
    Force disable updatable APEX
    
    * TARGET_FLATTEN_APEX is set to true if we are building unbundled apps, make sure it's disabled
    
    Change-Id: I1a88059e396f90f613d0c83b31f788802026dc12
    
    Also unset DEXPREOPT_GENERATE_APEX_IMAGE

--- device.mk
+++ device.mk
@@ -26,6 +26,8 @@ PRODUCT_USES_QCOM_HARDWARE := true
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+PRODUCT_PRODUCT_PROPERTIES += ro.apex.updatable=false
+
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
