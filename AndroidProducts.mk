1f07999066c3e72b76adaf7222e704a2c2533af8
GenZSouL <priyanshusinghal0818x@gmail.com>
2023-06-14 16:13:16 +0000

    RMX2061: DerpFest bringup
    Signed-off-by: GenZSouL <priyanshusinghal0818x@gmail.com>
    Signed-off-by: GenZSouL <priyanshusinghal0818ext@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX2061.mk
+    $(LOCAL_DIR)/derp_RMX2061.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX2061-user \
-    lineage_RMX2061-userdebug \
-    lineage_RMX2061-eng 
+    derp_RMX2061-user \
+    derp_RMX2061-userdebug \
+    derp_RMX2061-eng 
--- derp_RMX2061.mk
+++ derp_RMX2061.mk
@@ -10,18 +10,19 @@ $(call inherit-product, device/realme/RMX2061/device.mk)
 # Hotword
 $(call inherit-product, vendor/realme/hotword/google.mk)
 
-# ROM Flags
+# DerpFest Flags
 TARGET_BOOT_ANIMATION_RES := 1080
 WITH_GMS := true
+DERP_BUILDTYPE := Official
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_BUILD_APERTURE_CAMERA := true
+USE_LEGACY_BOOTANIMATION := false
 
 # Inherit some common stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Device identifier
-PRODUCT_NAME := lineage_RMX2061
+PRODUCT_NAME := derp_RMX2061
 PRODUCT_DEVICE := RMX2061
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX2061
