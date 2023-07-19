26db17a8a913c7e00f3059287e7c561c4c798aac
yograjfire18 <yograjfire18@gmail.com>
2023-07-19 18:58:39 +0530

    oplus: use oplus libperfmgr

--- device.mk
+++ device.mk
@@ -306,7 +306,11 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service-qti \
+    android.hardware.power@1.2.vendor \
+    vendor.qti.hardware.perf@2.2.vendor \
+    android.hardware.power-service.oplus-libperfmgr
+
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
