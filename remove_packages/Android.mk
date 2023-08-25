6e4775f82082f24d7547290994ae1cd6a4c64606
Neel <neeljadhavforcoc@gmail.com>
2023-08-25 18:33:04 +0530

    RMX2061: update `Removepackages`

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,13 +4,60 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService Camera2 ConnMO DCMO
-LOCAL_OVERRIDES_PACKAGES += DMService GCS GoogleCamera MaestroPrebuilt MicropaperPrebuilt MyVerizonServices
-LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions Ornament SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap
-LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM TurboPrebuilt Tycho USCCDM VZWAPNLib
-LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger WallpapersBReel2020 obdm_stub DevicePolicyPrebuilt RecorderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += Camera2 YouTube WellbeingPrebuilt AudioFX Recorder
+LOCAL_OVERRIDES_PACKAGES := \
+    AmbientSensePrebuilt \
+    AudioFX \
+    AppDirectedSMSService \
+    arcore \
+    CalendarGooglePrebuilt \
+    Camera2 \
+    Chrome \
+    Chrome-Stub \
+    ConnMO \
+    ConnMetrics \
+    DCMO \
+    DevicePolicyPrebuilt \
+    DMService \
+    Drive \
+    GCS \
+    GoogleTTS \
+    GoogleCamera \
+    MaestroPrebuilt \
+    MicropaperPrebuilt \
+    Maps \
+    MyVerizonServices \
+    OBDM_Permissions \
+    obdm_stub \
+    OdadPrebuilt \
+    OemDmTrigger \
+    OPScreenRecord \
+    Ornament \
+    Photos \
+    PixelBuds \
+    PixelLiveWallpaperPrebuilt \
+    PrebuiltGmail \
+    RecorderPrebuilt \
+    Recorder \
+    SafetyHubPrebuilt \
+    Snap \
+    SCONE \
+    ScribePrebuilt \
+    Showcase \
+    SoundAmplifierPrebuilt \
+    SprintDM \
+    SprintHM \
+    Tycho \
+    TurboPrebuilt \
+    USCCDM \
+    Via \
+    Videos \
+    VZWAPNLib \
+    VzwOmaTrigger \
+    WellbeingPrebuilt \
+    WallpapersBReel2020 \
+    YouTube \
+    YouTubeMusicPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
-LOCAL_CERTIFICATE := platform
+LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
 include $(BUILD_PREBUILT)
